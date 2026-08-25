c1b2d8db4cd41c820c393bbe2240137feb193849
Faragz <a.khaled46462@gmail.com>
2026-08-25 14:16:32 +0300

    init frontend & backend setup with swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = new DocumentBuilder()
+    .setTitle('Full-Stack Test API')
+    .setDescription('API documentation for the Full-Stack Test project')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api/docs', app, document);
+  const port = process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
